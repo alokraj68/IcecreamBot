8e9f9228537adbb388ec3e50bf76d8f412ca1af1
Alok Rajasukumaran <alokraj68@gmail.com>
2019-07-15 18:24:35 +0530

    Updating flows and fixing  flow issues

--- dialogs/orderDialog.js
+++ dialogs/orderDialog.js
@@ -1,10 +1,20 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { TimexProperty } = require('@microsoft/recognizers-text-data-types-timex-expression');
-const { ConfirmPrompt, TextPrompt, WaterfallDialog } = require('botbuilder-dialogs');
-const { CancelAndHelpDialog } = require('./cancelAndHelpDialog');
-const { DateResolverDialog } = require('./dateResolverDialog');
+const {
+    TimexProperty
+} = require('@microsoft/recognizers-text-data-types-timex-expression');
+const {
+    ConfirmPrompt,
+    TextPrompt,
+    WaterfallDialog
+} = require('botbuilder-dialogs');
+const {
+    CancelAndHelpDialog
+} = require('./cancelAndHelpDialog');
+const {
+    DateResolverDialog
+} = require('./dateResolverDialog');
 
 const CONFIRM_PROMPT = 'confirmPrompt';
 const DATE_RESOLVER_DIALOG = 'dateResolverDialog';
@@ -19,6 +29,7 @@ class OrderDialog extends CancelAndHelpDialog {
             .addDialog(new ConfirmPrompt(CONFIRM_PROMPT))
             .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
                 this.icecreamTypeStep.bind(this),
+                this.icecreamSizeStep.bind(this),
                 this.confirmStep.bind(this),
                 this.finalStep.bind(this)
             ]));
@@ -33,26 +44,30 @@ class OrderDialog extends CancelAndHelpDialog {
         const orderDetails = stepContext.options;
 
         if (!orderDetails.iceCreamType) {
-            return await stepContext.prompt(TEXT_PROMPT, { prompt: 'Which type of Icecream do you need? Cone or Cup' });
+            return await stepContext.prompt(TEXT_PROMPT, {
+                prompt: 'Which type of Icecream do you need? Cone or Cup'
+            });
         } else {
             return await stepContext.next(orderDetails.iceCreamType);
         }
     }
 
-    // /**
-    //  * If an origin city has not been provided, prompt for one.
-    //  */
-    // async originStep(stepContext) {
-    //     const orderDetails = stepContext.options;
+    /**
+     * If an origin city has not been provided, prompt for one.
+     */
+    async icecreamSizeStep(stepContext) {
+        const orderDetails = stepContext.options;
 
-    //     // Capture the response to the previous step's prompt
-    //     bookingDetails.destination = stepContext.result;
-    //     if (!bookingDetails.origin) {
-    //         return await stepContext.prompt(TEXT_PROMPT, { prompt: 'From what city will you be travelling?' });
-    //     } else {
-    //         return await stepContext.next(bookingDetails.origin);
-    //     }
-    // }
+        // Capture the response to the previous step's prompt
+        orderDetails.iceCreamType = stepContext.result;
+        if (orderDetails.iceCreamType.toLowerCase() == "cone" && !orderDetails.iceCreamSize) {
+            return await stepContext.prompt(TEXT_PROMPT, {
+                prompt: 'What size do you need? I have regular as well as large.'
+            });
+        } else {
+            return await stepContext.next(orderDetails.iceCreamSize);
+        }
+    }
 
     // /**
     //  * If a travel date has not been provided, prompt for one.
@@ -77,11 +92,18 @@ class OrderDialog extends CancelAndHelpDialog {
         const orderDetails = stepContext.options;
 
         // Capture the results of the previous step
-        orderDetails.iceCreamType = stepContext.result;
-        const msg = `Please confirm, You want a: ${ orderDetails.iceCreamType }`;
+        if (orderDetails.iceCreamType.toLowerCase().trim() == "cone") {
+            orderDetails.iceCreamSize = stepContext.result;
+        } else {
+            orderDetails.iceCreamType = stepContext.result;
+        }
+
+        const msg = (orderDetails.iceCreamType.toLowerCase().trim() == "cone") ? `Please confirm, You want a  ${ toCamelCase(orderDetails.iceCreamSize) }  ${ toCamelCase(orderDetails.iceCreamType) } ice cream` : `Please confirm, You want a: ${ toCamelCase(orderDetails.iceCreamType) } ice cream`;
 
         // Offer a YES/NO prompt.
-        return await stepContext.prompt(CONFIRM_PROMPT, { prompt: msg });
+        return await stepContext.prompt(CONFIRM_PROMPT, {
+            prompt: msg
+        });
     }
 
     /**
@@ -97,6 +119,24 @@ class OrderDialog extends CancelAndHelpDialog {
         }
     }
 
+
+
+}
+
+function toCamelCase(str) {
+    // Lower cases the string
+    return str.toLowerCase()
+        // Replaces any - or _ characters with a space 
+        .replace(/[-_]+/g, ' ')
+        // Removes any non alphanumeric characters 
+        .replace(/[^\w\s]/g, '')
+        // Uppercases the first character in each group immediately following a space 
+        // (delimited by spaces) 
+        .replace(/ (.)/g, function ($1) {
+            return $1.toUpperCase();
+        })
+    //   // Removes spaces 
+    //   .replace( / /g, '' );
 }
 
 module.exports.OrderDialog = OrderDialog;
