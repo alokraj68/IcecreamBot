ac8fd1b8e5cb0e19f0af236225ca8c250dbed8d4
Alok Rajasukumaran <alokraj68@gmail.com>
2019-07-16 18:11:19 +0530

    Update dialogAndWelcomeBot.js

--- bots/dialogAndWelcomeBot.js
+++ bots/dialogAndWelcomeBot.js
@@ -9,7 +9,7 @@ class DialogAndWelcomeBot extends DialogBot {
     constructor(conversationState, userState, dialog, logger) {
         super(conversationState, userState, dialog, logger);
 
-        this.onMembersAdded(async (context, next) => {
+        this.onConversationUpdate(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; cnt++) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
@@ -21,6 +21,18 @@ class DialogAndWelcomeBot extends DialogBot {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+        // this.onMembersAdded(async (context, next) => {
+        //     const membersAdded = context.activity.membersAdded;
+        //     for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+        //         if (membersAdded[cnt].id !== context.activity.recipient.id) {
+        //             const welcomeCard = CardFactory.adaptiveCard(IcecreamCard);
+        //             await context.sendActivity({ attachments: [welcomeCard] });
+        //         }
+        //     }
+
+        //     // By calling next() you ensure that the next BotHandler is run.
+        //     await next();
+        // });
     }
 }
 
